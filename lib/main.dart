a81e48fba84ef3ee0d561addd2291ce0660710f5
Harsha R <20cs015@ritrjpm.ac.in>
2023-09-18 20:37:57 +0530

    intial commit

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+
+import './screens/spashScreen.dart';
+
+import './const/colors.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AirQI',
+      theme: ThemeData(
+        fontFamily: "Metropolis",
+        primarySwatch: Colors.lightBlue,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(
+              AppColor.orange,
+            ),
+            shape: MaterialStateProperty.all(
+              StadiumBorder(),
+            ),
+            elevation: MaterialStateProperty.all(0),
+          ),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all(
+              AppColor.orange,
+            ),
+          ),
+        ),
+        textTheme: TextTheme(
+          headline3: TextStyle(
+            color: AppColor.primary,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+          headline4: TextStyle(
+            color: AppColor.secondary,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+          headline5: TextStyle(
+            color: AppColor.primary,
+            fontWeight: FontWeight.normal,
+            fontSize: 25,
+          ),
+          headline6: TextStyle(
+            color: AppColor.primary,
+            fontSize: 25,
+          ),
+          bodyText2: TextStyle(
+            color: AppColor.secondary,
+          ),
+        ),
+      ),
+      home: MyCustomWidget(),
+      // routes: {
+      //   LandingScreen.routeName: (context) => LandingScreen(),
+      //   LoginScreen.routeName: (context) => LoginScreen(),
+      //   SignUpScreen.routeName: (context) => SignUpScreen(),
+      //   ForgetPwScreen.routeName: (context) => ForgetPwScreen(),
+      //   SendOTPScreen.routeName: (context) => SendOTPScreen(),
+      //   NewPwScreen.routeName: (context) => NewPwScreen(),
+      //   IntroScreen.routeName: (context) => IntroScreen(),
+      // },
+    );
+  }
+}
